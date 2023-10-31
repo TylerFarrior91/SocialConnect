d26ee5363ddb6a64f8312984670f7adbf55c1e7a
Tyler Farrior <tylerfarrior@gmail.com>
2023-10-30 20:40:13 -0400

    updating index.js

--- Develop/routes/api/index.js
+++ Develop/routes/api/index.js
@@ -2,7 +2,7 @@ const router = require('express').Router();
 const courseRoutes = require('./usersRoutes');
 const studentRoutes = require('./thoughtsRoutes');
 
-router.use('/courses', courseRoutes);
+router.use('/thoughts', courseRoutes);
 router.use('/students', studentRoutes);
 
 module.exports = router;
