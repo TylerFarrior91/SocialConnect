3b8c6cde8c172d7dcf83b2f52d5bd6aeeebe46f4
Tyler Farrior <tylerfarrior@gmail.com>
2023-10-30 20:41:10 -0400

    adding router.use(user)

--- Develop/routes/api/index.js
+++ Develop/routes/api/index.js
@@ -2,7 +2,7 @@ const router = require('express').Router();
 const courseRoutes = require('./usersRoutes');
 const studentRoutes = require('./thoughtsRoutes');
 
-router.use('/thoughts', courseRoutes);
-router.use('/students', studentRoutes);
+router.use('/thoughts', thoughtsRoutes);
+router.use('/user', studentRoutes);
 
 module.exports = router;
