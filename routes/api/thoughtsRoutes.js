a3ebca0ea7cc286711247104b197dda16b7f9691
Tyler Farrior <tylerfarrior@gmail.com>
2023-11-01 21:56:34 -0400

    updating reactionId

--- routes/api/thoughtsRoutes.js
+++ routes/api/thoughtsRoutes.js
@@ -4,20 +4,20 @@ const {
   getSingleThoughts,
   createThoughts,
   deleteThoughts,
-  addThoughts,
-  removeAssignment,
+  addReaction,
+  removeReaction,
 } = require('../../controllers/userController');
 
-// /api/students
+
 router.route('/').get(getThoughts).post(createThoughts);
 
-// /api/students/:studentId
+
 router.route('/:userId').get(getSingleThoughts).delete(deleteThoughts;
 
-// /api/students/:studentId/assignments
-router.route('/:studentId/assignments').post(addAssignment);
 
-// /api/students/:studentId/assignments/:assignmentId
-router.route('/:studentId/assignments/:assignmentId').delete(removeAssignment);
+router.route('/:thoughtId/reactions').post(addAssignment);
+
+
+router.route('/:thoughtId/reaction/:reactionId').delete(removeAssignment);
 
 module.exports = router;
