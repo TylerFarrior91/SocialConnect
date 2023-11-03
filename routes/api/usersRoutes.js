9aa782871052cbb0ce9742af605cec988dc6ad41
Tyler Farrior <tylerfarrior@gmail.com>
2023-11-03 16:57:21 -0400

    adding add/remove friend function

--- routes/api/usersRoutes.js
+++ routes/api/usersRoutes.js
@@ -1,19 +1,21 @@
 const router = require('express').Router();
 const {
-  getUser,
+  getUsers,
   getSingleUser,
   createUser,
-  updateUser,
   deleteUser,
-} = require('../../controllers/thoughtsController.js');
+  updateUser,
+  addFriend,
+  removeFriend,
+} = require('../../controllers/userController');
 
 
-router.route('/').get(getUser).post(createUser);
+router.route('/').get(getUsers).post(createUser);
 
-router
-  .route('/:courseId')
-  .get(getSingleUser)
-  .put(updateUser)
-  .delete(deleteUser);
+
+router.route('/:userId').get(getSingleUser).delete(deleteUser).put(updateUser);
+
+
+router.route('/:userId/friends/:friendId').delete(removeFriend).post(addFriend);
 
 module.exports = router;
